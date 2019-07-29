9429c7cdac77115ad8003c4404b2cbf749d13922
Bogdan Cismariu <bogdan.cismariu@gmail.com>
2019-07-29 16:07:37 +0300

    accept installs

--- install-php73.sh
+++ install-php73.sh
@@ -4,7 +4,7 @@
 #----------------
 
 # Install Remi Repo
-yum install http://rpms.remirepo.net/enterprise/remi-release-7.rpm
+yum install -y http://rpms.remirepo.net/enterprise/remi-release-7.rpm
 yum install -y yum-utils
 yum-config-manager --disable remi-php54
 yum-config-manager --enable remi-php73
